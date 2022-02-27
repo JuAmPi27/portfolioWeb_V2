1e5ca828600e6e9ba4d7c9e82700ff1a32303cda
Juampi27 <Jpbarnetche27@gmail.com>
2022-02-27 20:33:44 -0300

    componente skills y proyectos

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  githubUrl = "https://github.com/JuAmPi27?tab=repositories";
 
   constructor() { }
 
