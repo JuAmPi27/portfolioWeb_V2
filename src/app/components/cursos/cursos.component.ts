d568b0cd3dd9ff0031b08332dba34c218b527d37
Juampi27 <Jpbarnetche27@gmail.com>
2022-03-25 17:31:20 -0300

    datos dinamizados en cursos y educacion y algunas modificaciones más

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosPortfolioService } from 'src/app/services/datos-portfolio.service';
 
 @Component({
   selector: 'app-cursos',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  cursosList: any;
 
-  constructor() { }
+  constructor(private datosPortfolio: DatosPortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe( data => {
+      this.cursosList = data.cursos;
+    });
   }
 
 }
